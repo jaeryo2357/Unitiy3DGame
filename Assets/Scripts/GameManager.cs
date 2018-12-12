e84ff78e9c70b7f6df0400ea54c107111038057a
jaeryo <jaeryo2357@naver.com>
2018-12-12 19:03:55 +0900

    camera moving

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,14 +6,40 @@ public class GameManager : MonoBehaviour {
 
     GameObject Hp;
     GameObject HpText;
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
+    public GameObject playerButton;
+    public GameObject HpUi;
     int playerHp;
 	// Use this for initialization
 	void Start () {
         Hp = GameObject.Find("Hpbar");
         HpText = GameObject.Find("HpbarText");
         playerHp = 100;
+        ShowOverheadView();
+      
 	}
-	
+    public void ShowOverheadView()
+    {
+        firstPersonCamera.enabled = false;
+        overheadCamera.enabled = true;
+        playerButton.SetActive(true);
+        HpUi.SetActive(false);
+    }
+
+    public void ShowFirstPersonView()
+    {
+        firstPersonCamera.enabled = true;
+        overheadCamera.enabled = false;
+        playerButton.SetActive(false);
+        HpUi.SetActive(true);
+    }
+
+    public void GamePlay()
+    {
+        ShowFirstPersonView();
+    }
+
 
     public void playerAttacked(int power)
     {
